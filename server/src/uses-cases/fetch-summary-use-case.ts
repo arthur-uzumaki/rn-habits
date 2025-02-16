882375e2124f15fbc244235852b80ffa994e0c5b
arthur-uzumaki <arthursousarabelo@gmail.com>
2025-02-15 22:08:47 -0300

    fix: summary query to correctly display completed habits

--- server/src/uses-cases/fetch-summary-use-case.ts
+++ server/src/uses-cases/fetch-summary-use-case.ts
@@ -7,24 +7,24 @@ interface FetchSummaryUseCaseRequest {
 export class FetchSummaryUseCase {
   async execute({ userId }: FetchSummaryUseCaseRequest) {
     const summary = await prisma.$queryRaw /*sql*/`
-      SELECT 
-        D.id,
-        D.date,
-        CAST(COUNT(DH.id) AS float) AS completed,
-        CAST(COUNT(HDW.id) AS float) AS amount
-      FROM 
-        days D
-      LEFT JOIN 
-        day_habits DH ON DH.day_id = D.id
-      LEFT JOIN 
-        habit_week_days HDW ON EXTRACT(DOW FROM D.date) = HDW.week_day
-      LEFT JOIN 
-        habits H ON H.id = HDW.habit_id 
-      WHERE 
-        H."userId" = ${userId} 
-      GROUP BY 
-        D.id, D.date
-    `
+    SELECT 
+      D.id,
+      D.date,
+      CAST(COUNT(DISTINCT DH.id) AS float) AS completed,
+      CAST(COUNT(DISTINCT H.id) AS float) AS amount
+    FROM 
+      days D
+    LEFT JOIN 
+      day_habits DH ON DH.day_id = D.id
+    LEFT JOIN 
+      habit_week_days HDW ON EXTRACT(DOW FROM D.date) = HDW.week_day
+    LEFT JOIN 
+      habits H ON H.id = HDW.habit_id AND H."userId" = ${userId}
+    WHERE 
+      H.id IS NOT NULL
+    GROUP BY 
+      D.id, D.date
+  `
 
     return summary
   }
